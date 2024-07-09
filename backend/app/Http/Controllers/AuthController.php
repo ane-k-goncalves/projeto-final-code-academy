0c4c8845325a3d188f3a3680390b572ab20b2868
MiroTragico12 <jorge.santista10@hotmail.com>
2024-07-09 15:49:24 -0300

    finalizacao do backend login

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
-use App\Models\User;
+
 
 class AuthController extends Controller
 {
