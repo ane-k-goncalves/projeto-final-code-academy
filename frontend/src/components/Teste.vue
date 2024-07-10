137f096ad1995b10ced07fc443fd29e067e91f00
MiroTragico12 <jorge.santista10@hotmail.com>
2024-07-09 23:05:02 -0300

    implementacao inicial do vue draggable

---
+++ frontend/src/components/Teste.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+      <h1>Favorite Foods</h1>
+      <draggable v-model="meals" tag="ul" group="meals" :animation="300">
+        <template #item="{ element: meal }">
+          <li>{{ meal }}</li>
+        </template>
+      </draggable>
+  
+      <h1>Terrible Foods</h1>
+      <draggable v-model="yuckyMeals" tag="ul" group="meals" :animation="300">
+        <template #item="{ element: meal }">
+          <li>{{ meal }}</li>
+        </template>
+      </draggable>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  import draggable from 'vuedraggable';
+  
+  const meals = ref([
+    'Pizza',
+    'Sushi',
+    'Pasta'
+  ]);
+  
+  const yuckyMeals = ref([
+    'Bat wing soup',
+    'Spicy Octopus',
+    'Anything from Taco Bell',
+  ]);
+  </script>
+  
+  <style scoped>
+  /* Adicione estilos específicos para o componente Teste aqui, se necessário */
+  </style>
+  
